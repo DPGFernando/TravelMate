f07310301373e769c1555dba4772b66910dac925
Thilini-2000 <tweerakumara@gmail.com>
2024-09-02 19:57:14 +0530

    edit_loginpage

---
+++ app/src/main/java/com/example/travelmate/LoginActivity.java
@@ -0,0 +1,12 @@
+package com.example.travelmate;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);  // Ensure you have a layout file named activity_login.xml
+    }
+}
--- app/src/main/java/com/example/travelmate/MainActivity.java
+++ app/src/main/java/com/example/travelmate/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.travelmate;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    private MaterialButton loginButton, createAccountButton;
+    private ImageView logoImageView;
+    private TextView travelMateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.loginbtn);
+        createAccountButton = findViewById(R.id.createbtn);
+        logoImageView = findViewById(R.id.imageView);
+        travelMateTextView = findViewById(R.id.travelmate);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent signUpIntent = new Intent(MainActivity.this, com.example.travelmate.SignUpActivity.class);
+                startActivity(signUpIntent);
+            }
         });
     }
 }
