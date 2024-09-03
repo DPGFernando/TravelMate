3d26226bd73fa96b6432d323a2722feaa50be986
DPGFernando <dinethrag11@gmail.com>
2024-09-03 17:25:35 +0530

    signupform_eventmanager
    creating signup form for event manager

---
+++ app/src/main/java/com/example/travelmate/Interface.java
@@ -0,0 +1,52 @@
+package com.example.travelmate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Interface extends AppCompatActivity {
+
+    Button logOutBtn;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_interface);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = fAuth.getCurrentUser();
+        if(!user.isEmailVerified()){
+            Toast.makeText(Interface.this, "Email is not verified", Toast.LENGTH_SHORT).show();
+            /*Intent i = new Intent(this, signup_eventManager.class);
+            startActivity(i);*/
+        }
+
+        logOutBtn = findViewById(R.id.logOut);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), signup_eventManager.class));
+        finish();
+    }
+}
