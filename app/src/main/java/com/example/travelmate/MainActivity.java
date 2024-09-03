34049c9276ae1a2894d7eac14ecea12fb810c9c3
DManamperi <dulminithathsarani1999@gmail.com>
2024-09-03 12:14:17 +0530

    LogIn

--- app/src/main/java/com/example/travelmate/MainActivity.java
+++ app/src/main/java/com/example/travelmate/MainActivity.java
@@ -1,28 +1,29 @@
 package com.example.travelmate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // Find the button and blank space view by their IDs
+        //Button buttonClick = findViewById(R.id.button_click);
 
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        /* Set an OnClickListener for the button
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+           /* @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, loggin.class);
+                startActivity(intent);
+            }*/
+        }
     }
-}
