100bca2679122ce0860889eee4e10fe0db32fe27
DPGFernando <dinethrag11@gmail.com>
2024-10-16 12:41:56 +0530

    package details part

---
+++ app/src/main/java/com/example/travelmate/packageDetails.java
@@ -0,0 +1,105 @@
+package com.example.travelmate;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+public class packageDetails extends AppCompatActivity {
+
+    TextView packageTitle, packageDes, packagePrice, packageDays;
+    ImageView packageImage;
+    StorageReference storageReference;
+    FirebaseFirestore fStore;
+    FirebaseAuth fAuth;
+    String userID;
+    Button editPackage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_package_details);
+
+        packageTitle = findViewById(R.id.packageTitle);
+        packageDes = findViewById(R.id.packageDes);
+        packagePrice = findViewById(R.id.packagePrice);
+        packageDays = findViewById(R.id.packageDays);
+        packageImage = findViewById(R.id.packageImage);
+        editPackage = findViewById(R.id.editPackage);
+        fAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        fStore = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("TouristGuide").document("C1N5cGNvLiWSkCqkJ9qyVfVcoOi2");
+        StorageReference fileRef = storageReference.child("Packages/C1N5cGNvLiWSkCqkJ9qyVfVcoOi2/Kabaragala.jpg");
+
+        fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(packageImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("TAG", "onFailure: " + e.getMessage());
+            }
+        });
+
+        DocumentReference documentReference1 = documentReference.collection("packages").document("AdIAJPf4mNAuWnoFJw0f");
+        documentReference1.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                packageTitle.setText(value.getString("packageName"));
+                packageDes.setText(value.getString("description"));
+                packagePrice.setText(value.getString("price"));
+                packageDays.setText(value.getString("noOfDays"));
+            }
+        });
+
+        editPackage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(packageDetails.this, editpackageDetails.class);
+                intent.putExtra("packageID", "AdIAJPf4mNAuWnoFJw0f");
+                intent.putExtra("touristGuideID", "C1N5cGNvLiWSkCqkJ9qyVfVcoOi2");
+                intent.putExtra("packageTitle", packageTitle.getText().toString());
+                intent.putExtra("packageDes", packageDes.getText().toString());
+                intent.putExtra("packagePrice", packagePrice.getText().toString());
+                intent.putExtra("packageDays", packageDays.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
