454422dc0cdf57ddf5cad6f3fa567c6559444b6e
DPGFernando <129350824+DPGFernando@users.noreply.github.com>
2024-10-23 23:40:11 +0530

    starting 4th phase

---
+++ app/src/main/java/com/example/travelmate/touristMain.java
@@ -0,0 +1,72 @@
+package com.example.travelmate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.travelmate.databinding.ActivityTouristGuideMainBinding;
+import com.example.travelmate.databinding.ActivityTouristMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class touristMain extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    ActivityTouristMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tourist_main);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = fAuth.getCurrentUser();
+        if(!user.isEmailVerified()){
+            Toast.makeText(touristMain.this, "Email is not verified", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, loggin.class);
+            startActivity(i);
+        }
+
+        binding = ActivityTouristMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.events) {
+                replaceFragment(new EventFragment());
+            } else if (itemId == R.id.touristGuide) {
+                replaceFragment(new TouristGuideFragment());
+            } else if (itemId == R.id.map) {
+                replaceFragment(new MapFragment());
+            } else if (itemId == R.id.contact) {
+                replaceFragment(new ContactFragment());
+            }
+
+            return true;
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.replace_layout, fragment).commit();
+    }
+}
